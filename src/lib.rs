406940db8b7c84c2a590c9d8ee1ab24eb8a21247
Tangtang Zhou <tangtang2995@gmail.com>
2024-09-07 11:18:47 +0200

    feat: implement battle structure map into bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::io::Cursor;
 
-use binrw::prelude::*;
+use binrw::{BinRead, BinWrite};
 
-#[derive(BinRead, Debug)]
+#[derive(BinRead, BinWrite, Debug)]
+#[brw(little)]
 pub struct PackedBattleStructure {
     stage_id: u8,
     flags: u8,
@@ -21,7 +22,8 @@ pub struct PackedBattleStructure {
     enemy_level: [u8; 8],
 }
 
-#[derive(BinRead, Debug, Clone)]
+#[derive(BinRead, BinWrite, Debug, Clone)]
+#[brw(little)]
 pub struct Coordinate {
     x: i16,
     y: i16,
@@ -59,7 +61,7 @@ pub struct CameraAttributes {
 }
 
 /// Enemy information where id is equal to PackedBattleStructure.id_enemies[idx] - 0x10
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Enemy {
     id: u8,
     level: u8,
@@ -74,60 +76,61 @@ pub struct Enemy {
     unknown_4: u8,
 }
 
-pub fn parse_battle_structure(bytes: &[u8]) -> anyhow::Result<PackedBattleStructure> {
-    let mut cursor = Cursor::new(bytes);
-    let battle_structure_packed: PackedBattleStructure = cursor.read_le()?;
-    Ok(battle_structure_packed)
-}
+impl PackedBattleStructure {
 
-impl From<u8> for BattleFlags {
-    fn from(value: u8) -> Self {
-        Self {
-            cannot_escape: (value & (1 << 0)) > 0,
-            disable_win_fanfare: (value & (1 << 1)) > 0,
-            show_timer: (value & (1 << 2)) > 0,
-            no_exp: (value & (1 << 3)) > 0,
-            disable_exp_screen: (value & (1 << 4)) > 0,
-            force_surprise_attack: (value & (1 << 5)) > 0,
-            force_back_attack: (value & (1 << 6)) > 0,
-            scripted_battle: (value & (1 << 7)) > 0,
-        }
+    pub fn try_from_bytes(bytes: &[u8]) -> anyhow::Result<PackedBattleStructure> {
+        let mut cursor = Cursor::new(bytes);
+        let battle_structure_packed = PackedBattleStructure::read(&mut cursor)?;
+        Ok(battle_structure_packed)
     }
-}
 
-impl From<u8> for CameraAttributes {
-    fn from(value: u8) -> Self {
-        Self {
-            number: value >> 4,
-            animation: value & 0xF,
-        }
-    }
-}
-
-impl From<PackedBattleStructure> for BattleStructure {
-    fn from(value: PackedBattleStructure) -> Self {
-
+    pub fn into_battle_structure(self) -> BattleStructure {
         BattleStructure {
-            stage_id: value.stage_id,
-            flags: value.flags.into(),
-            main_camera: value.main_camera.into(),
-            secondary_camera: value.secondary_camera.into(),
+            stage_id: self.stage_id,
+            flags: self.battle_flags(),
+            main_camera: self.main_camera(),
+            secondary_camera: self.secondary_camera(),
             enemies: [
-                value.enemy(0),
-                value.enemy(1),
-                value.enemy(2),
-                value.enemy(3),
-                value.enemy(4),
-                value.enemy(5),
-                value.enemy(6),
-                value.enemy(7),
+                self.enemy(0),
+                self.enemy(1),
+                self.enemy(2),
+                self.enemy(3),
+                self.enemy(4),
+                self.enemy(5),
+                self.enemy(6),
+                self.enemy(7),
             ]
         }
     }
-}
 
-impl PackedBattleStructure {
-    pub fn enemy(&self, index: usize) -> Enemy {
+    fn main_camera(&self) -> CameraAttributes {
+        CameraAttributes {
+            number: self.main_camera >> 4,
+            animation: self.main_camera & 0xF,
+        }
+    }
+
+    fn secondary_camera(&self) -> CameraAttributes {
+        CameraAttributes {
+            number: self.secondary_camera >> 4,
+            animation: self.secondary_camera & 0xF,
+        }
+    }
+
+    fn battle_flags(&self) -> BattleFlags {
+        BattleFlags {
+            cannot_escape: (self.flags & (1 << 0)) > 0,
+            disable_win_fanfare: (self.flags & (1 << 1)) > 0,
+            show_timer: (self.flags & (1 << 2)) > 0,
+            no_exp: (self.flags & (1 << 3)) > 0,
+            disable_exp_screen: (self.flags & (1 << 4)) > 0,
+            force_surprise_attack: (self.flags & (1 << 5)) > 0,
+            force_back_attack: (self.flags & (1 << 6)) > 0,
+            scripted_battle: (self.flags & (1 << 7)) > 0,
+        }
+    }
+
+    fn enemy(&self, index: usize) -> Enemy {
         let mask = 0x80 >> index;
 
         Enemy {
@@ -144,6 +147,78 @@ impl PackedBattleStructure {
             unknown_4: self.unknown_4[index],
         }
     }
+}
+
+impl BattleStructure {
+    pub fn as_packed_bytes(&self) -> anyhow::Result<Vec<u8>> {
+        let packed_battle_structure = PackedBattleStructure {
+            stage_id: self.stage_id,
+            flags: self.packed_battle_flags(),
+            main_camera: (self.main_camera.number << 4) | self.main_camera.animation,
+            secondary_camera: (self.secondary_camera.number << 4) | self.secondary_camera.animation,
+            not_visible_enemies: self.packed_not_visible_enemies(),
+            not_loaded_enemies: self.packed_not_loaded_enemies(),
+            not_targetable_enemies: self.packed_untargetable_enemies(),
+            enabled_enemies: self.packed_enabled_enemies(),
+            enemies_coords: self.enemies.each_ref().map(|enemy| enemy.coordinate.clone()),
+            id_enemies: self.enemies.each_ref().map(|enemy| enemy.id + 0x10),
+            unknown_1: self.enemies.each_ref().map(|enemy| enemy.unknown_1),
+            unknown_2: self.enemies.each_ref().map(|enemy| enemy.unknown_2),
+            unknown_3: self.enemies.each_ref().map(|enemy| enemy.unknown_3),
+            unknown_4: self.enemies.each_ref().map(|enemy| enemy.unknown_4),
+            enemy_level: self.enemies.each_ref().map(|enemy| enemy.level),
+        };
+
+        let mut writer = Cursor::new(Vec::new());
+        packed_battle_structure.write(&mut writer)?;
+        Ok(writer.into_inner())
+    }
+
+    fn packed_battle_flags(&self) -> u8 {
+        let mut flags = 0u8;
+        flags |= self.flags.cannot_escape as u8;
+        flags |= (self.flags.disable_win_fanfare as u8) << 1;
+        flags |= (self.flags.show_timer as u8) << 2;
+        flags |= (self.flags.no_exp as u8) << 3;
+        flags |= (self.flags.disable_exp_screen as u8) << 4;
+        flags |= (self.flags.force_surprise_attack as u8) << 5;
+        flags |= (self.flags.force_back_attack as u8) << 6;
+        flags |= (self.flags.scripted_battle as u8) << 7;
+        flags
+    }
+
+    fn packed_not_visible_enemies(&self) -> u8 {
+        let mut not_visible_enemies = 0u8;
+        for (i, enemy) in self.enemies.iter().enumerate() {
+            not_visible_enemies |= (enemy.invisible as u8) << (self.enemies.len() - 1 - i);
+        }
+        not_visible_enemies
+    }
+
+    fn packed_not_loaded_enemies(&self) -> u8 {
+        let mut not_loaded_enemies = 0u8;
+        for (i, enemy) in self.enemies.iter().enumerate() {
+            not_loaded_enemies |= (enemy.not_loaded as u8) << (self.enemies.len() - 1 - i);
+        }
+        not_loaded_enemies
+    }
+
+    fn packed_enabled_enemies(&self) -> u8 {
+        let mut enabled_enemies = 0u8;
+        for (i, enemy) in self.enemies.iter().enumerate() {
+            enabled_enemies |= (enemy.enabled as u8) << (self.enemies.len() - 1 - i);
+        }
+        enabled_enemies
+    }
+
+    fn packed_untargetable_enemies(&self) -> u8 {
+        let mut untargetable_enemies = 0u8;
+        for (i, enemy) in self.enemies.iter().enumerate() {
+            untargetable_enemies |= (enemy.untargetable as u8) << (self.enemies.len() - 1 - i);
+        }
+        untargetable_enemies
+    }
+
 }
 
 #[cfg(test)]
@@ -175,15 +250,15 @@ mod test {
 
     #[test]
     fn test_parser() {
-        let packed_battle_structure = parse_battle_structure(BYTES).unwrap();
+        let packed_battle_structure = PackedBattleStructure::try_from_bytes(BYTES).unwrap();
         println!("{:?}", packed_battle_structure);
         assert_eq!(packed_battle_structure.stage_id, 6);
     }
 
     #[test]
     fn test_parse_battle_structure() {
-        let packed_battle_structure = parse_battle_structure(BYTES).unwrap();
-        let battle_structure = BattleStructure::from(packed_battle_structure);
+        let packed_battle_structure = PackedBattleStructure::try_from_bytes(BYTES).unwrap();
+        let battle_structure = packed_battle_structure.into_battle_structure();
         println!("{:?}", battle_structure);
         assert_eq!(battle_structure.stage_id, 6);
 
@@ -228,6 +303,14 @@ mod test {
         assert_eq!(battle_structure.enemies[4].unknown_2, 0xc8);
         assert_eq!(battle_structure.enemies[4].unknown_3, 0xea60);
         assert_eq!(battle_structure.enemies[4].unknown_4, 0x2);
+    }
+
+
+    #[test]
+    fn test_parser_and_writer() {
+        let packed_battle_structure = PackedBattleStructure::try_from_bytes(BYTES).unwrap();
+        let battle_structure = packed_battle_structure.into_battle_structure();
+        assert_eq!(battle_structure.as_packed_bytes().unwrap(), BYTES);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use ff8_battle_structure_gui::{BattleStructure, PackedBattleStructure};
 use iced::{
-    executor, font::Family, widget::{button, checkbox, column, pick_list, row, text}, Application, Command, Font, Settings
+    executor,
+    font::Family,
+    widget::{button, checkbox, column, pick_list, row, text},
+    Application, Command, Font, Settings,
 };
 use rfd::AsyncFileDialog;
+
+const BATTLE_STRUCTURE_NUMBER: usize = 1000;
 
 fn main() -> iced::Result {
     MyApp::run(Settings::default())
@@ -15,6 +21,7 @@ enum AppMessage {
 
 struct MyApp {
     counter: usize,
+    battle_structure: Option<[BattleStructure; BATTLE_STRUCTURE_NUMBER]>,
 }
 
 impl Application for MyApp {
@@ -24,26 +31,24 @@ impl Application for MyApp {
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (Self { counter: 0 }, Command::none())
+        (Self { counter: 0, battle_structure: None }, Command::none())
     }
 
     fn title(&self) -> String {
         String::from("My App")
     }
 
-    fn update(&mut self, message: Self::Message) -> Command::<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
             AppMessage::ButtonPressed => {
-                let pick_files = AsyncFileDialog::new()
-                    .add_filter("text", &["txt", "rs"])
-                    .add_filter("rust", &["rs", "toml"])
-                    .set_directory("/")
-                    .pick_file();
-                Command::perform(pick_files, |_| {
-                    AppMessage::DoNothing
+                Command::perform(read_scene_out_file(), |battle_structure_list| {
+                    match battle_structure_list {
+                        Ok(battle_structure_list) => AppMessage::DoNothing,
+                        Err(_) => AppMessage::DoNothing,
+                    }
                 })
-            },
-            AppMessage::DoNothing => Command::none()
+            }
+            AppMessage::DoNothing => Command::none(),
         }
     }
 
@@ -70,5 +75,23 @@ impl Application for MyApp {
     fn theme(&self) -> iced::Theme {
         iced::Theme::Dark
     }
+}
 
+async fn read_scene_out_file() -> anyhow::Result<Vec<BattleStructure>> {
+    let file_handle = AsyncFileDialog::new()
+        .add_filter("text", &["txt", "rs"])
+        .add_filter("rust", &["rs", "toml"])
+        .set_directory("/")
+        .pick_file()
+        .await
+        .ok_or(anyhow::anyhow!("File not found"))?;
+
+    let bytes = file_handle.read().await;
+    let mut battle_structure_list = Vec::with_capacity(BATTLE_STRUCTURE_NUMBER);
+    for i in 0..BATTLE_STRUCTURE_NUMBER {
+        let offset = i * size_of::<PackedBattleStructure>();
+        let packed_bs = PackedBattleStructure::try_from_bytes(&bytes[offset..offset + size_of::<PackedBattleStructure>()])?;
+        battle_structure_list.push(packed_bs.into_battle_structure());
+    }
+    Ok(battle_structure_list)
 }
